97d19277116f7393f3b7c3f20cf820cb8281dcaf
Junaid <rajali069@gmail.com>
2024-04-20 23:50:36 +0530

    first commit

---
+++ src/page/index.jsx
@@ -0,0 +1,201 @@
+import React from 'react';
+import './index.css';
+
+export default function Main() {
+  return (
+    <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
+      <div className='w-[1408px] h-[621px] bg-[url(../assets/images/0e13cab8f4f8005b8eb2bfb6bd00e5a1b541b1cd.png)] bg-cover bg-no-repeat absolute bottom-[30.029px] right-[20.275px] z-[2]'>
+        <div className='w-[1406px] h-[79px] relative z-[58] mt-[26px] mr-0 mb-0 ml-[-1px]'>
+          <div className='w-[1406px] h-[68px] bg-[url(../assets/images/2dd51eda393015c2d36a855210e729b8a04402a1.png)] bg-cover bg-no-repeat absolute bottom-[11px] right-0 z-[57]' />
+          <div className='w-[1396px] h-[65px] bg-[rgba(0,0,0,0)] absolute bottom-0 right-[5px] z-[58]'>
+            <div className='flex w-[109px] h-[16px] justify-between items-center relative z-[66] mt-[5px] mr-0 mb-0 ml-[27px]'>
+              <div className='w-[16px] h-[16px] shrink-0 bg-[url(../assets/images/42a7a1b81341e53dbe2c3534b4259be14662a0fd.png)] bg-cover bg-no-repeat relative z-[66]' />
+              <span className="h-[12px] shrink-0 font-['Inter'] text-[9px] font-bold leading-[12px] text-[#7f8cad] relative text-left whitespace-nowrap z-[65]">
+                REAL ESTATE(1)
+              </span>
+            </div>
+            <div className='w-[1294px] h-[13px] relative z-[64] mt-[16px] mr-0 mb-0 ml-[27px]'>
+              <span className="flex h-[13px] justify-start items-center font-['Inter'] text-[9px] font-bold leading-[10.892px] text-[#aebfcf] absolute bottom-0 right-[1177px] text-left whitespace-nowrap z-[64]">
+                NAME OF THE HOLDING
+              </span>
+              <span className="flex h-[13px] justify-start items-center font-['Inter'] text-[9px] font-bold leading-[10.892px] text-[#afc0d0] absolute bottom-0 right-[844px] text-left whitespace-nowrap z-[63]">
+                TICKER
+              </span>
+              <span className="flex h-[13px] justify-start items-center font-['Inter'] text-[9px] font-bold leading-[10.892px] text-[#adbfcf] absolute bottom-0 right-[550px] text-left whitespace-nowrap z-[62]">
+                AVERAGEPRICE
+              </span>
+              <span className="flex h-[13px] justify-start items-center font-['Inter'] text-[9px] font-bold leading-[10.892px] text-[#adbece] absolute bottom-0 right-[421px] text-left whitespace-nowrap z-[61]">
+                MARKET PRICE
+              </span>
+              <span className="flex h-[13px] justify-start items-center font-['Inter'] text-[9px] font-bold leading-[10.892px] text-[#afc0d0] absolute bottom-0 right-[219px] text-left whitespace-nowrap z-[60]">
+                LATEST CHANGE PERCENTAGE
+              </span>
+              <span className="flex h-[12px] justify-start items-center font-['Inter'] text-[9px] font-bold leading-[10.892px] text-[#aebfcf] absolute bottom-px right-0 text-left whitespace-nowrap z-[59]">
+                MARKET VALUE IN BASE CCY
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className='w-[1394px] h-[47px] bg-[url(../assets/images/4a61cb2acc5ce3ae5cafccabfeb8db3aeffbf5eb.png)] bg-cover bg-no-repeat relative z-[56] mt-[-4px] mr-0 mb-0 ml-[4px]'>
+          <span className="flex h-[14px] justify-start items-center font-['Inter'] text-[9px] font-normal leading-[10.892px] text-[#9ca6be] absolute bottom-[18px] right-[1072px] text-left whitespace-nowrap z-[56]">
+            Property: The Sail @ Marina Bay Unit 68-020000 3BR 1991sqft
+          </span>
+          <span className="flex h-[14px] justify-start items-center font-['Inter'] text-[10px] font-normal leading-[12.102px] text-[#9aa4bd] absolute bottom-[18px] right-[517px] text-left whitespace-nowrap z-[53]">
+            3996971.1
+          </span>
+          <span className="flex h-[13px] justify-start items-center font-['Inter'] text-[10px] font-normal leading-[12.102px] text-[#98a2bc] absolute bottom-[19px] right-[164px] text-left whitespace-nowrap z-[51]">
+            3996971.1
+          </span>
+          <span className="flex h-[13px] justify-start items-center font-['Inter'] text-[9px] font-normal leading-[10.892px] text-[#9ca6be] absolute bottom-[18px] right-[783px] text-left whitespace-nowrap z-[55]">
+            Property_TheSail@MarinaBay-68-02
+          </span>
+          <span className="flex h-[13px] justify-start items-center font-['Inter'] text-[10px] font-normal leading-[12.102px] text-[#96a1bb] absolute bottom-[18px] right-[658px] text-left whitespace-nowrap z-[54]">
+            3982000
+          </span>
+          <span className="flex h-[12px] justify-start items-center font-['Inter'] text-[10px] font-normal leading-[12px] text-[#9aa4be] absolute bottom-[19px] right-[428px] text-left whitespace-nowrap z-[52]">
+            31
+          </span>
+        </div>
+        <div className='flex w-[79px] h-[17px] justify-between items-center relative z-[49] mt-[26px] mr-0 mb-0 ml-[31px]'>
+          <div className='w-[17px] h-[17px] shrink-0 bg-[url(../assets/images/3dac49a3e70a604a49eb97f634c82b63545f3d45.png)] bg-cover bg-no-repeat relative z-[49]' />
+          <span className="h-[12px] shrink-0 font-['Inter'] text-[9px] font-normal leading-[12px] text-[#818daf] relative text-left whitespace-nowrap z-[48]">
+            CASH(16)
+          </span>
+        </div>
+        <div className='flex w-[79px] h-[16px] justify-between items-center relative z-[47] mt-[36px] mr-0 mb-0 ml-[31px]'>
+          <div className='w-[16px] h-[16px] shrink-0 bg-[url(../assets/images/24052a742299d8315e2a8030ad117e441467131f.png)] bg-cover bg-no-repeat rounded-[6.5px] relative z-[47]' />
+          <span className="h-[13px] shrink-0 font-['Inter'] text-[9px] font-normal leading-[13px] text-[#7686aa] relative text-left whitespace-nowrap z-[46]">
+            BOND (45)
+          </span>
+        </div>
+        <div className='flex w-[87.275px] h-[16px] justify-between items-center relative z-[45] mt-[34.699px] mr-0 mb-0 ml-[30.725px]'>
+          <div className='w-[16px] h-[16px] shrink-0 bg-[url(../assets/images/24052a742299d8315e2a8030ad117e441467131f.png)] bg-cover bg-no-repeat rounded-[6.5px] relative z-[3]' />
+          <span className="h-[14px] shrink-0 font-['Inter'] text-[9px] font-normal leading-[14px] text-[#8793b3] relative text-left whitespace-nowrap z-[45]">
+            EQUITY (35)
+          </span>
+        </div>
+        <div className='w-[1402px] h-[261px] relative z-[9] mt-[9.301px] mr-0 mb-0 ml-0'>
+          <div className='w-[1398px] h-[261px] bg-[url(../assets/images/3f36feed588de8ce69fb7650af5c43e7a9b1bc6a.png)] bg-cover bg-no-repeat absolute bottom-0 right-[4px] z-[4]'>
+            <div className='flex w-[73px] h-[16px] justify-between items-center relative z-[44] mt-[29px] mr-0 mb-0 ml-[31px]'>
+              <div className='w-[16px] h-[16px] shrink-0 bg-[url(../assets/images/1f32a795c36d552ca112f752dd4d2576480beb66.png)] bg-cover bg-no-repeat relative z-[44]' />
+              <span className="h-[13px] shrink-0 font-['Inter'] text-[9px] font-normal leading-[13px] text-[#7c89ad] relative text-left whitespace-nowrap z-[43]">
+                FUND (4)
+              </span>
+            </div>
+            <div className='w-[1261px] h-[14px] relative z-[42] mt-[15px] mr-0 mb-0 ml-[31px]'>
+              <span className="flex h-[14px] justify-start items-center font-['Inter'] text-[9px] font-bold leading-[10.892px] text-[#b0c0d1] absolute bottom-0 right-[653px] text-left whitespace-nowrap z-40">
+                AVERAGEPRICE
+              </span>
+              <span className="flex h-[14px] justify-start items-center font-['Inter'] text-[9px] font-bold leading-[10.892px] text-[#adbfcf] absolute bottom-0 right-[475px] text-left whitespace-nowrap z-[39]">
+                MARKETPRICE
+              </span>
+              <span className="flex h-[13px] justify-start items-center font-['Inter'] text-[9px] font-bold leading-[10.892px] text-[#afc0d0] absolute bottom-0 right-[1145px] text-left whitespace-nowrap z-[42]">
+                NAME OF THE HOLDING
+              </span>
+              <span className="flex h-[12px] justify-start items-center font-['Inter'] text-[9px] font-bold leading-[10.892px] text-[#afbfcf] absolute bottom-px right-[902px] text-left whitespace-nowrap z-[41]">
+                TICKER
+              </span>
+              <span className="flex h-[13px] justify-start items-center font-['Inter'] text-[9px] font-bold leading-[10.892px] text-[#b0c1d1] absolute bottom-0 right-[253px] text-left whitespace-nowrap z-[38]">
+                LATEST CHANGE PERCENTAGE
+              </span>
+              <span className="flex h-[13px] justify-start items-center font-['Inter'] text-[9px] font-bold leading-[10.892px] text-[#afc0d0] absolute bottom-0 right-0 text-left whitespace-nowrap z-[37]">
+                MARKET VALUEIN BASE CCY
+              </span>
+            </div>
+            <div className='w-[1389px] h-[44px] bg-[rgba(0,0,0,0)] relative z-30 mt-[12px] mr-0 mb-0 ml-[8px]'>
+              <span className="flex h-[13px] justify-start items-center font-['Inter'] text-[9px] font-normal leading-[10.892px] text-[#9fa9c0] absolute bottom-[16px] right-[1237px] text-left whitespace-nowrap z-[36]">
+                Gel Greatlink China Growth
+              </span>
+              <span className="flex h-[13px] justify-start items-center font-['Inter'] text-[9px] font-normal leading-[10.892px] text-[#9ea8c0] absolute bottom-[16px] right-[744px] text-left whitespace-nowrap z-[34]">
+                1.00555555555556
+              </span>
+              <span className="flex h-[13px] justify-start items-center font-['Inter'] text-[9px] font-normal leading-[10.892px] text-[#9ba5be] absolute bottom-[16px] right-[980px] text-left whitespace-nowrap z-[35]">
+                GELCHGR_SP
+              </span>
+              <span className="flex h-[12px] justify-start items-center font-['Inter'] text-[10px] font-normal leading-[12px] text-[#9ea8c0] absolute bottom-[17px] right-[202px] text-left whitespace-nowrap z-[31]">
+                353001.6
+              </span>
+              <span className="flex h-[13px] justify-start items-center font-['Inter'] text-[10px] font-normal leading-[12.102px] text-[#9faac2] absolute bottom-[16px] right-[626px] text-left whitespace-nowrap z-[33]">
+                1.751
+              </span>
+              <span className="flex h-[12px] justify-start items-center font-['Inter'] text-[8px] font-normal leading-[9.682px] text-[#f99faa] absolute bottom-[17px] right-[490px] text-left whitespace-nowrap z-[32]">
+                -45
+              </span>
+            </div>
+            <div className='w-[1386px] h-[43px] bg-[rgba(0,0,0,0)] relative z-[23] mt-[-4px] mr-0 mb-0 ml-[9px]'>
+              <span className="flex h-[14px] justify-start items-center font-['Inter'] text-[9px] font-normal leading-[10.892px] text-[#a1aac1] absolute bottom-[16px] right-[927px] text-left whitespace-nowrap z-[28]">
+                GELREAL_SP+23%CORP
+              </span>
+              <span className="flex h-[14px] justify-start items-center font-['Inter'] text-[10px] font-normal leading-[12.102px] text-[#9fa9c0] absolute bottom-[16px] right-[736px] text-left whitespace-nowrap z-[27]">
+                0.788051744413955
+              </span>
+              <span className="flex h-[13px] justify-start items-center font-['Inter'] text-[9px] font-normal leading-[10.892px] text-[#9fa8c0] absolute bottom-[17px] right-[200px] text-left whitespace-nowrap z-[24]">
+                228569.6
+              </span>
+              <span className="flex h-[13px] justify-start items-center font-['Inter'] text-[9px] font-normal leading-[10.892px] text-[#a1aac1] absolute bottom-[16px] right-[1173px] text-left whitespace-nowrap z-[29]">
+                Greatlink Global Real Estate Corporation
+              </span>
+              <span className="flex h-[12px] justify-start items-center font-['Inter'] text-[10px] font-normal leading-[12px] text-[#9ca5be] absolute bottom-[17px] right-[623px] text-left whitespace-nowrap z-[26]">
+                0.896
+              </span>
+              <span className="flex h-[12px] justify-start items-center font-['Inter'] text-[10px] font-normal leading-[12px] text-[#a3acc1] absolute bottom-[17px] right-[491px] text-left whitespace-nowrap z-[25]">
+                20
+              </span>
+            </div>
+            <div className='w-[1391px] h-[42px] bg-[rgba(0,0,0,0)] relative z-[16] mt-[-4px] mr-0 mb-0 ml-[7px]'>
+              <span className="flex h-[14px] justify-start items-center font-['Inter'] text-[9px] font-normal leading-[10.892px] text-[#9ba5be] absolute bottom-[14px] right-[979px] text-left whitespace-nowrap z-[21]">
+                GELGROW_SP
+              </span>
+              <span className="flex h-[13px] justify-start items-center font-['Inter'] text-[10px] font-normal leading-[12.102px] text-[#a0a9c1] absolute bottom-[15px] right-[211px] text-left whitespace-nowrap z-[17]">
+                173810
+              </span>
+              <span className="flex h-[12px] justify-start items-center font-['Inter'] text-[9px] font-normal leading-[10.892px] text-[#9ba5be] absolute bottom-[15px] right-[1240px] text-left whitespace-nowrap z-[22]">
+                Gel Greatlink Global Equity
+              </span>
+              <span className="flex h-[12px] justify-start items-center font-['Inter'] text-[10px] font-normal leading-[12px] text-[#9ea8c0] absolute bottom-[15px] right-[626px] text-left whitespace-nowrap z-[19]">
+                1.337
+              </span>
+              <span className="flex h-[12px] justify-start items-center font-['Inter'] text-[9px] font-normal leading-[10.892px] text-[#9da7bf] absolute bottom-[15px] right-[810px] text-left whitespace-nowrap z-20">
+                0.759
+              </span>
+              <span className="flex h-[12px] justify-start items-center font-['Inter'] text-[12px] font-normal leading-[12px] text-[#8793b1] absolute bottom-[15px] right-[499px] text-left whitespace-nowrap z-[18]">
+                4
+              </span>
+            </div>
+          </div>
+          <div className='w-[1398px] h-[42px] bg-[rgba(0,0,0,0)] absolute bottom-[16px] right-0 z-[9]'>
+            <span className="flex h-[13px] justify-start items-center font-['Inter'] text-[9px] font-normal leading-[10.892px] text-[#99a3bd] absolute bottom-[14px] right-[1248px] text-left whitespace-nowrap z-[15]">
+              Gel Greatlink Global Bond
+            </span>
+            <span className="flex h-[13px] justify-start items-center font-['Inter'] text-[9px] font-normal leading-[10.892px] text-[#9da6be] absolute bottom-[14px] right-[987px] text-left whitespace-nowrap z-[14]">
+              GELGLBF_SP
+            </span>
+            <span className="flex h-[12px] justify-start items-center font-['Inter'] text-[10px] font-normal leading-[12px] text-[#9fa8bf] absolute bottom-[15px] right-[215px] text-left whitespace-nowrap z-10">
+              123200
+            </span>
+            <span className="flex h-[12px] justify-start items-center font-['Inter'] text-[10px] font-normal leading-[12px] text-[#9ba5be] absolute bottom-[14px] right-[814px] text-left whitespace-nowrap z-[13]">
+              1.429
+            </span>
+            <span className="flex h-[12px] justify-start items-center font-['Inter'] text-[10px] font-normal leading-[12px] text-[#97a2bc] absolute bottom-[14px] right-[636px] text-left whitespace-nowrap z-[12]">
+              1.76
+            </span>
+            <span className="flex h-[11px] justify-start items-center font-['Inter'] text-[10px] font-normal leading-[11px] text-[#8e9ab5] absolute bottom-[15px] right-[498px] text-left whitespace-nowrap z-[11]">
+              47
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className='h-[660px] bg-[url(../assets/images/e6c0e5b7a8b55c4f6619bed1a4a187d806e2473f.png)] bg-cover bg-no-repeat absolute top-[15.699px] left-[-0.27px] right-[0.27px] z-[1]' />
+      <div className='h-[644px] bg-[rgba(0,0,0,0)] absolute top-[15.699px] left-[-0.27px] right-[0.27px]'>
+        <div className='w-[1440px] h-[101px] bg-[rgba(0,0,0,0)] relative z-[5] mt-[543px] mr-0 mb-0 ml-0'>
+          <div className='w-[1405px] h-[60px] bg-[url(../assets/images/b906e09cb54f2c25cf30b985301dc23e323fd5e8.png)] bg-cover bg-no-repeat relative z-[6] mt-[9px] mr-0 mb-0 ml-[14px]'>
+            <span className="flex h-[13px] justify-start items-center font-['Inter'] text-[9px] font-bold leading-[10.892px] text-[#7c8aad] absolute bottom-[26px] right-[1304px] text-left whitespace-nowrap z-[7]">
+              LOAN(6)
+            </span>
+            <div className='w-[15px] h-[15px] bg-[url(../assets/images/ec1c9dbcb33f930bbe6a1c662c68375679bce7e4.png)] bg-cover bg-no-repeat rounded-[7.5px] absolute bottom-[23px] right-[1361px] z-[8]' />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
